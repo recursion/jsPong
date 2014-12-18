4a877d9cb1b932ba283f62aac0ef23ad35a433bc
Michael Symmes <msymmes@gmail.com>
2014-12-17 20:42:33 -0500

    Basic paddle controls workinggit checkout -b playerInput

--- js/game.js
+++ js/game.js
@@ -9,9 +9,16 @@ function Paddle(side){
   if(typeof side === 'undefined'){
     return 'FAIL! - MUST use side argument (\'left\') or (\'right\')';
   }
+  // paddles must have a side!('left' or 'right')
   this.side = side;
+  
   this.width = 25;
   this.height = 100;
+
+  // Paddles only move up and down
+  this.velY = 0;
+  this.speed = 4;
+  this.maxVelocity = 12;
 
   // Put the paddle right in the middle of the y-axis
   this.y = (canvas.height / 2) - (this.height / 2);
@@ -25,12 +32,19 @@ function Paddle(side){
     this.x = canvas.width - this.width;
     this.color = '#0000ff';
   }
-  // Paddles only move up and down
-  this.velY = 0;
-  this.maxVelocity = 5;
 
   this.update = function(){
-    console.log('Updating ' + this.side);
+    //console.log('Updating ' + this.side + ' velY: ' + this.velY);
+    console.log(this.velY);
+
+    // Check for screen boundries
+    if(this.y + this.velY <= 0){
+      this.y = 0;
+    } else if (this.y + this.velY + this.height >= canvas.height){
+      this.y = canvas.height - this.height;
+    } else {
+      this.y += this.velY;
+    }
   };
 
   this.draw = function(){
@@ -70,7 +84,8 @@ function Game(){
     };
 
     var update = function(){
-      
+      player1.update();
+      player2.update();
     };
     var draw = function(){
       //console.log('giDrawing');
@@ -108,80 +123,91 @@ function Game(){
         draw();
       }
     };
+
+   /*********************************************************
+   *
+   *                KEYBOARD EVENT HANDLERS
+   *
+   * The following two functions handle onkeydown and onkeyup events
+   * we grab the keycode and respond to it if needed.
+   * keycodes are unicode 
+   ********************************************************/
+  // For smoother input it makes sense to capture keydown and keyup events
+  // and set movement accordingly.
+  this.onKeyDownEvent = function(event){
+    var chCode = ('which' in event) ? event.which : event.keyCode;
+    switch(chCode){
+      case 87:
+        console.log('w pressed');
+        if (Math.abs(player1.velY) < player1.maxVelocity){
+          player1.velY -= player1.speed;
+        }
+        break;
+      case 83:
+        console.log('s pressed');
+        if (Math.abs(player1.velY) < player1.maxVelocity){
+          player1.velY += player1.speed;
+        }
+        break;
+      case 38:
+        console.log('Up arrow pressed');
+        if (Math.abs(player2.velY) < player2.maxVelocity){
+          player2.velY -= player2.speed;
+        }
+        break;
+      case 40:
+        console.log('Down arrow pressed');
+        if (Math.abs(player2.velY) < player2.maxVelocity){
+          player2.velY += player2.speed;
+        }
+        break;
+      
+      case 32:
+        console.log('Space pressed');
+
+      default:
+        console.log('Unhandled Key: ' + chCode);
+    }
+
+  }
+  this.onKeyUpEvent = function(event){
+    var chCode = ('which' in event) ? event.which : event.keyCode;
+    switch(chCode){
+      case 87:
+        console.log('w released');
+        player1.velY = 0; 
+        break;
+
+      case 83:
+        console.log('s released');
+        player1.velY = 0; 
+        break;
+      
+      case 38:
+        console.log('Up arrow released');
+        player2.velY = 0; 
+        break;
+
+      case 40:
+        console.log('Down arrow released');
+        player2.velY = 0; 
+        break;
+
+      case 32:
+        console.log('Space released');
+        break;
+
+
+      default:
+        console.log('Unhandled Key: ' + chCode);
+    }
+
+  }
+
   }
 }
 
-/*********************************************************
- *
- *                KEYBOARD EVENT HANDLERS
- *
- * The following two functions handle onkeydown and onkeyup events
- * we grab the keycode and respond to it if needed.
- * keycodes are unicode 
- ********************************************************/
-// For smoother input it makes sense to capture keydown and keyup events
-// and set movement accordingly.
-function onKeyDownEvent(event){
-  var chCode = ('which' in event) ? event.which : event.keyCode;
-  switch(chCode){
-    case 87:
-      console.log('w pressed');
-      game.player1.velY--;
-      break;
-    case 83:
-      console.log('s pressed');
-      game.player1.velY++;
-      break;
-    case 38:
-      console.log('Up arrow pressed');
-      game.player2.velY--;
-      break;
-    case 40:
-      console.log('Down arrow pressed');
-      game.player2.velY++;
-      break;
-    
-    case 32:
-      console.log('Space pressed');
 
-    default:
-      console.log('Unhandled Key: ' + chCode);
-  }
-
-}
-function onKeyUpEvent(event){
-  var chCode = ('which' in event) ? event.which : event.keyCode;
-  switch(chCode){
-    case 87:
-      console.log('w released');
-      game.player1.velY++;
-      break;
-
-    case 83:
-      console.log('s released');
-      game.player1.velY--;
-      break;
-    
-    case 38:
-      console.log('Up arrow released');
-      game.player2.velY++;
-      break;
-
-    case 40:
-      console.log('Down arrow released');
-      game.player2.velY--;
-      break;
-
-    case 32:
-      console.log('Space released');
-      break;
-
-
-    default:
-      console.log('Unhandled Key: ' + chCode);
-  }
-
-}
 // respond to window size changes
 function resizeCanvas(){
       canvas.width = window.innerWidth;
@@ -198,6 +224,9 @@ resizeCanvas();
 
 //  Create a new game 
 var game = new Game();
+document.querySelector('body').onkeyup = game.onKeyUpEvent;
+document.querySelector('body').onkeydown = game.onKeyDownEvent;
+
 if(typeof game.fps === 'undefined'){
   console.log('giDid not get a proper game object');
 } else {
